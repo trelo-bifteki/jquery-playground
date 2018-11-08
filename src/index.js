add876bd130ed0f0d0fc62ae68486405f2b9aa4f
Lampros Papadimitriou <papadimitriou@tyntec.com>
2018-11-08 18:27:04 +0100

    adding jquery and playing around

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-export default username => `Hello ${username}`;
+import $ from 'jquery';
+
+console.info('Hey there'); // eslint-disable-line no-console
+
+const handler = $(() => {
+  console.info('document loaded'); // eslint-disable-line no-console
+});
+
+export default handler;
